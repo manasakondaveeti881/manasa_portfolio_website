c6fb1c80da2222ad8bfd8dcda32b67f1c6d72dd4
Manasa <manasakondaveeti881@gmail.com>
2026-07-05 20:25:25 +0530

    Configured Railway environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.hayabusa.proxy.rlwy.net,
-    user: process.env.root,
-    password: process.env.CUSXfqIlUexjCrIeaRyYpnFyaUystjUD,
-    database: process.env.railway,
-    port: process.env41392
+    host: "hayabusa.proxy.rlwy.net",
+    user: "root",
+    password: "CUSXfqIlUexjCrIeaRyYpnFyaUystjUD",
+    database: "railway",
+    port: 41392
 });
 
 connection.connect((err) => {
     if (err) {
-        console.log("Database connection failed");
+        console.log("❌ Database connection failed");
         console.log(err);
     } else {
-        console.log("Connected to Railway MySQL");
+        console.log("✅ Connected to Railway MySQL");
     }
 });
 
